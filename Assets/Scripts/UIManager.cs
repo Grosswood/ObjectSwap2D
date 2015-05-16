ac3769772e2b5c80c3b7a9aa1d015b916dba7372
grosswood <mozge4ok@gmail.com>
2015-05-16 16:02:00 +0600

    Full swapper
    Signed-off-by: grosswood <mozge4ok@gmail.com>

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,6 +17,8 @@ public class UIManager : MonoBehaviour {
 	private int secondObj;
 	private bool itemDragged = false;
 
+	private int[] matrOrder = new int[6]  { 0, 1, 2, 3, 4, 5 };
+	private int[] shadowOrder = new int[6] { 0, 5, 4, 3, 2, 1 };
 
 	void Start () 
 	{
@@ -30,14 +32,8 @@ public class UIManager : MonoBehaviour {
 	{
 		if (Input.GetMouseButton (0))
 		{
-			if (itemDragged)
-			{
+			if (itemDragged && firstObj > 0)
 				image[firstObj].transform.position =Vector3.MoveTowards (image[firstObj].transform.position, Input.mousePosition, 100f);
-			}
-
-
-			//image1.transform.position = Input.mousePosition;
-			//image1.transform.position = defPos[0];
 		}
 
 		if (Input.GetMouseButtonDown (0))
@@ -49,15 +45,10 @@ public class UIManager : MonoBehaviour {
 		if (Input.GetMouseButtonUp (0))
 		{
 			secondObj = calculatePosition();
+			swapPoses(firstObj, secondObj);
 
-			defPos[0] = defPos[firstObj];
-			defPos[firstObj] = defPos[secondObj];
-			defPos[secondObj] = defPos[0];
-
-			if (secondObj == 0)
-			{
+			if (secondObj == 0)  //if dragged to nowhere just go back
 				secondObj = firstObj;
-			}
 
 			itemDragged = false;
 		}
@@ -69,47 +60,31 @@ public class UIManager : MonoBehaviour {
 			image[3].transform.position = Vector3.MoveTowards ( image[3].transform.position, defPos[3], 10f);
 			image[4].transform.position = Vector3.MoveTowards ( image[4].transform.position, defPos[4], 10f);
 			image[5].transform.position = Vector3.MoveTowards ( image[5].transform.position, defPos[5], 10f);
-
-			//image[firstObj].transform.position = Vector3.MoveTowards (image[firstObj].transform.position, defPos[secondObj], 10f);
 		}
+
+		matchCheck ();
 	}
 
 	//compare MousePos with DefPos and return number of DefPos if any nearby
 	int calculatePosition ()
 	{
-		if (Mathf.Abs (Input.mousePosition.x - defPos [1].x) < 50 &&
-			Mathf.Abs (Input.mousePosition.y - defPos [1].y) < 50)
+		if 		(Mathf.Abs (Input.mousePosition.x - defPos [1].x) < 50 &&
+		      	 Mathf.Abs (Input.mousePosition.y - defPos [1].y) < 50)
 			return 1;
 		else if (Mathf.Abs (Input.mousePosition.x - defPos [2].x) < 50 &&
-			Mathf.Abs (Input.mousePosition.y - defPos [2].y) < 50)
+		         Mathf.Abs (Input.mousePosition.y - defPos [2].y) < 50)
 			return 2;
 		else if (Mathf.Abs (Input.mousePosition.x - defPos [3].x) < 50 &&
-			Mathf.Abs (Input.mousePosition.y - defPos [3].y) < 50)
+		         Mathf.Abs (Input.mousePosition.y - defPos [3].y) < 50)
 			return 3;
 		else if (Mathf.Abs (Input.mousePosition.x - defPos [4].x) < 50 &&
-			Mathf.Abs (Input.mousePosition.y - defPos [4].y) < 50)
+		         Mathf.Abs (Input.mousePosition.y - defPos [4].y) < 50)
 			return 4;
 		else if (Mathf.Abs (Input.mousePosition.x - defPos [5].x) < 50 &&
-			Mathf.Abs (Input.mousePosition.y - defPos [5].y) < 50)
+		         Mathf.Abs (Input.mousePosition.y - defPos [5].y) < 50)
 			return 5;
 		else
 			return 0;
-		
-		
-		/*if ( Mathf.Abs(Input.mousePosition.x - defPos [0].x) + 50 &&
-			Input.mousePosition.x > defPos [0].x - 50 &&
-			Input.mousePosition.y < defPos [0].y + 50 &&
-			Input.mousePosition.y > defPos [0].y - 50)
-			return 1;
-		else if (Input.mousePosition.x < defPos [0].x + 50 &&
-		         Input.mousePosition.x < defPos [0].x + 50 &&
-		         Input.mousePosition.x < defPos [0].x + 50 &&
-		         Input.mousePosition.x < defPos [0].x + 50)
-			return 1;
-		else
-			return 0;
-*/
-
 
 	}
 
@@ -121,6 +96,27 @@ public class UIManager : MonoBehaviour {
 		defPos[3] = image[3].transform.position;
 		defPos[4] = image[4].transform.position;
 		defPos[5] = image[5].transform.position;
+	}
+
+	void swapPoses(int v1, int v2)
+	{
+		defPos[0] = defPos[v1];
+		defPos[v1] = defPos[v2];
+		defPos[v2] = defPos[0];
+
+		matrOrder[0] = matrOrder[v1];
+		matrOrder[v1] = matrOrder[v2];
+		matrOrder[v2] = matrOrder[0];
+	}
+
+	void matchCheck ()
+	{
+		if (matrOrder [1] == shadowOrder [1] &&
+		    matrOrder [2] == shadowOrder [2] &&
+		    matrOrder [3] == shadowOrder [3] &&
+		    matrOrder [4] == shadowOrder [4] &&
+		    matrOrder [5] == shadowOrder [5])
+			image[0].transform.position = Vector3.MoveTowards ( image[0].transform.position, defPos[1], 10f);
 	}
 
 }
